e05b972fa21bbf622afa8fb37dbcef81f0fe1f3e
vickman787 <cozyfabulous@gmail.com>
2026-07-24 10:58:54 +0100

    docs: update embedded video guide link

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -15,7 +15,7 @@ export default function DocsPage() {
         {/* Video Guide */}
         <div className="relative w-full aspect-video rounded overflow-hidden shadow-lg border border-[var(--color-border-subtle)]">
           <iframe
-            src="https://www.youtube.com/embed/XpMZf3W5E5A"
+            src="https://www.youtube.com/embed/yTErl_OAeyI"
             title="CiteFlowAI Video Guide"
             className="absolute top-0 left-0 w-full h-full"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
